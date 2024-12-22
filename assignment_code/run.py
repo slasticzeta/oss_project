579454d9a22bf0f53a18cd86d0b2e379d4586c70
fog0000 <wngp5366@gmail.com>
2024-12-22 23:56:27 +0900

    Reflect review

--- assignment_code/run.py
+++ assignment_code/run.py
@@ -34,6 +34,14 @@ def create_blocks():
             color = config.colors[color_index]
             block = Block(color, (x, y))
             BLOCKS.append(block)
+            
+def handle_item_effect(item):  # 아이템 효과를 처리하는 함수
+    global BALLS
+    if item.effect == "red":  # 빨간색 아이템 효과: 공 추가
+        BALLS.append(Ball((paddle.rect.centerx, paddle.rect.top)))
+    elif item.effect == "blue":  # 파란색 아이템 효과: 모든 공 복제 (예시)
+        for ball in BALLS[:]:
+            BALLS.append(Ball(ball.rect.center))
 
 
 def tick():
@@ -44,6 +52,7 @@ def tick():
     global paddle
     global ball1
     global start
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
